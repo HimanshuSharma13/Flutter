de6da87c8f129bdb889c7bb815a8a5e76e3c0a11
HimanshuSharmaHS <sharma.himanshu13391@gmail.com>
2021-06-24 11:12:20 +0530

    Getting android battery level

--- android/app/src/main/kotlin/com/learn/flutter_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/learn/flutter_app/MainActivity.kt
@@ -36,13 +36,15 @@ class MainActivity : FlutterActivity() {
 
 
     private fun getBatteryLevel(): Int {
-        val batteryLevel: IntVERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        val batteryLevel: Int
+        if(android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             val batteryManager = getSystemService(Context.BATTERY_SERVICE) as BatteryManager
             batteryLevel = batteryManager.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY)
         } else {
             val intent = ContextWrapper(applicationContext).registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
             batteryLevel = intent!!.getIntExtra(BatteryManager.EXTRA_LEVEL, -1) * 100 / intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
-        }       return batteryLevel
+        }
+        return batteryLevel
     }
 
 
